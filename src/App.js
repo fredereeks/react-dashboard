77c513f5f2b72a78aec873e57f17583cbadd2dfb
Frederick Jones <adefredy1@gmail.com>
2023-10-07 22:12:54 -0700

    With all the dependency packages installed and the tailwind configuration set up, commence the creation of an Admin Panel with React and TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+function App() {
+  
+  return (
+    <div className="dashboard">
+      <h1>Hello Admin Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
